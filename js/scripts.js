f43af24f5f467ec8079509bd54d771608dea0ef3
Leif Tabernero <latabernero@gmail.com>
2021-10-16 07:59:03 -1000

    Week 4 Assignment

--- js/scripts.js
+++ js/scripts.js
@@ -9,6 +9,12 @@ $(function () {
             $(".carousel").carousel("cycle");
             $("#carouselButton").children("i").removeClass("fa-play");
             $("#carouselButton").children("i").addClass("fa-pause"); 
-        }
+        }    
+    });
+    $("#reserveButton").click(function(){
+        $('#reserveModal').modal('show');
+    });
+    $("#loginButton").click(function(){
+        $('#loginModal').modal('show');
     });
 });
